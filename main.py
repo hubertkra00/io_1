4d58d18c6592b581373ac881889e21f150d24b2d
Hubert Krajewski <hubertk0725@gmail.com>
2024-03-24 12:43:03 +0100

    pole trapezu

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 bok_a = 8
 pole_kw = (bok_a)**2
 print(pole_kw)
+
+def pole_trapezu(a, b, h):
+    return 0.5 * (a + b) * h
+
+podstawa_a = 123
+podstawa_b = 999
+wysokosc = 89
+
+wynik = pole_trapezu(podstawa_a, podstawa_b, wysokosc)
+print("Pole trapezu wynosi:", wynik)
+
